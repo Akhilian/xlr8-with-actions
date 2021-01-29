08468d4a62c2cfac90250835ecc057bb85705032
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2021-01-29 12:22:47 +0100

    A release candidate tag should be created on push

--- add-release-candidate-tag.sh
+++ add-release-candidate-tag.sh
@@ -1,4 +1,24 @@
-echo "Applying a new tag"
-sha_of_commit=$(git rev-parse --short HEAD)
-echo $sha_of_commit
-echo ${{github.sha}}
+# Informations about the committer
+INPUT_COMMIT_USER_NAME="Adrien Saunier"
+INPUT_COMMIT_USER_EMAIL=""
+
+# Informations about the release tag
+shortened_commit_sha=${GITHUB_SHA::8}
+release_tag_name="RC-${shortened_commit_sha}"
+
+message="Applying a new release candidate tag 🏷 ${release_tag_name}"
+
+
+# Initialize git configuration
+git config --local user.name "$INPUT_COMMIT_USER_NAME"
+git config --local user.email "$INPUT_COMMIT_USER_EMAIL"
+
+
+# Create the tag
+echo $message
+
+git tag \
+    --annotate $release_tag_name \
+    -m "$message"
+
+git push --tags
